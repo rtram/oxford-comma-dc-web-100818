7264a8a148668e91ded53b1d10196746317a53d1
Robin Tram <robin.tram@gmail.com>
2018-09-19 17:44:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,9 @@ def oxford_comma(array)
     first_part = array[0].to_s
     last_part = array[-1].to_s
     "#{first_part} and #{last_part}"
+    elsif array.length > 2 
+    first_part = array[0...-1].join(", ")
+    last_part = array[-1].to_s
+    "#{first_part} and #{last_part}"
   end 
 end
