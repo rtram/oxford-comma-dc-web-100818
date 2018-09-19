3e0fa2e8e4723794145cfa8d9e9ccf95da29f111
Robin Tram <robin.tram@gmail.com>
2018-09-19 17:46:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+    elsif array.length == 2 
+    first_part = array[0].to_s
+    last_part = array[-1].to_s
+    "#{first_part} and #{last_part}"
+    elsif array.length > 2 
+    first_part = array[0...-1].join(", ")
+    last_part = array[-1].to_s
+    "#{first_part}, and #{last_part}"
+  end 
 end
