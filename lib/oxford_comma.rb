71b2f6b69dc18b315452867c5e05124212dc79da
Robin Tram <robin.tram@gmail.com>
2018-09-19 17:32:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   if array.length == 1
     array.join
     elsif array.length > 1 
-    array[0...-1].join
+    first_part = array[0...-1].join(", ")
+    last_part = array[-1].join
+    "#{first_part} and #{last_part}"
   end 
 end
